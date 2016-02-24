0688416a31a3daa2eb06aba2c15b25d384fe44c7
Lucas Rodrigues <lucas.rodrigues@descomplica.com.br>
2016-02-24 17:48:24 -0300

    Improve name of folder, and return after complete

--- src/dispatchers/dispatcher.js
+++ src/dispatchers/dispatcher.js
@@ -15,7 +15,7 @@ export default class Dispatcher {
           return UrlSetDispatcher.do(newBasePath, content.urlSets);
         })
         .then(() => {
-          resolve();
+          resolve(newBasePath);
         })
         .catch(err => {
           rejected(err);
@@ -36,10 +36,18 @@ export default class Dispatcher {
   }
 
   static _folderName() {
-    const date = new Date();
+    const addZeroIfLessThen10 = (value){
+      return value < 10 ? "0" + value : value;
+    }
 
-    return `${date.getFullYear()}_${date.getMonth() + 1}_` +
-      `${date.getDate()}_${date.getHours()}_${date.getMinutes()}_` +
-      `${date.getSeconds()}_sitemap`;
+    const date = new Date();
+    const year = date.getFullYear();
+    const month = addZeroIfLessThen10(date.getMonth());
+    const day = addZeroIfLessThen10(date.getDate());
+    const hours = addZeroIfLessThen10(date.getHours());
+    const minutes = addZeroIfLessThen10(date.getMinutes());
+    const seconds = addZeroIfLessThen10(date.getSeconds());
+
+    return `${year}${month}${day}${hours}${minutes}${seconds}_sitemap`;
   }
 }
